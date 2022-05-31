14321410fd5030b604b8c91095e128a610419f5f
Mian Talha <miantalhafazal@gmail.com>
2022-05-31 13:19:07 +0500

    Account setting page completed

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+let signedInUser;
+let current_user;
 var pages = {
-    'home': `<center><p class="text-justify">Welcome to to-do Lists. Sign up and get started today,<br>or Log in and pick up where you left!</p></center>
-    <div class="btn-group d-flex justify-content-center"  aria-label="Basic example"><center>
+    'home': `<center><h4 class="text-justify">Welcome to to-do Lists. Sign up and get started today,<br>or Log in and pick up where you left!</h4></center>
+    <div class="btn-group d-flex justify-content-center mt-3"  aria-label="Basic example"><center>
     <button type="button" class="btn btn-secondary" onclick="getPageContent('SignUp')">SignUp</button>
     <button type="button" class="btn btn-secondary" onclick="getPageContent('LogIn')">LogIn</button></center>
   </div>`,
-    'LogIn': `<form>
+    'LogIn': `<form style='border:1px solid black; background-color:rgb(106,133,255); padding:10px;'>
     <div class="mb-3">
       <label for="exampleInputEmail1" class="form-label">Email address</label>
       <input type="email" class="form-control" id="email">
@@ -16,7 +18,10 @@ var pages = {
     </div>
     <button type="submit" class="btn btn-primary" onclick="event.preventDefault();LogIn()">Submit</button>
   </form>`,
-    'SignUp': ` <form>
+    'SignUp': `<div class="mb-4 mt-4">
+    <h3>Create Your Account</h3>
+</div>
+     <form style='border:1px solid black; background-color:rgb(137,165,255); padding:10px;'>
     <div class="mb-3">
         <label for="Name" class="form-label">First Name</label>
         <input type="text" class="form-control" id="FirstName" required>
@@ -28,7 +33,6 @@ var pages = {
     <div class="mb-3">
         <label for="Email" class="form-label">Email address</label>
         <input type="email" class="form-control" id="Email" required>
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
     </div>
     <div class="mb-3">
         <label for="Password" class="form-label" >Password</label>
@@ -45,7 +49,7 @@ var pages = {
     'DashBoard': `
     <div class="mininav position-absolute top-0 ">
         <label id="loggedInUser"></label>|
-        <a href="#" class="text-decoration-none" onclick="getPageContent('AccountSettings')">
+        <a href="#" class="text-decoration-none" onclick="AccountSettings()">
                 Account Settings
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"></path>
@@ -60,39 +64,52 @@ var pages = {
         </a>
     </div>`,
     'TodoApp': ``,
-    'AccountSettings': `
+    'AccountSettings': `<div class="mininav position-absolute top-0 " style="margin-left: 480px;">
+    <label id="loggedInUser"></label>|
+    <a href="#" class="text-decoration-none" onclick="AccountSettings()">
+            Account Settings
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"></path>
+            </svg>
+    </a>|
+    <a href="#" class="text-decoration-none" onclick="LogOut()">
+          LogOut
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+             <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+            </svg>
+    </a>
+</div>
     <div class="mb-2">
         <h2 id='Edit_Info'></h2>
     </div>
     <div class="mb-2">
         <h6 style="color:blue;">Update Your Account Settings Here!</h6>
     </div>
-    <form>
+    <form style="border:1px solid black; padding:8px; background-color:rgb(162,217,255);">
     <div class="mb-2">
         <label for="Name" class="form-label">First Name</label>
-        <input type="text" class="form-control" id="FirstName" required>
+        <input type="text" class="form-control" id="updateFirstName" required>
     </div>
     <div class="mb-2">
         <label for="LastName" class="form-label">Last Name</label>
-        <input type="text" class="form-control" id="LastName" required>
+        <input type="text" class="form-control" id="updateLastName" required>
     </div>
     <div class="mb-2">
         <label for="Email" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="Email" required>
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <input type="email" class="form-control" id="updateEmail" required>
     </div>
     <div class="mb-2">
-        <label for="Password" class="form-label" >New Password</label>
-        <input type="password" class="form-control" id="NewPassword" minlength="8"  required>
+        <label for="NewPassword" class="form-label" >New Password</label>
+        <input type="text" class="form-control" id="NewPassword" minlength="8"  required>
     </div>
     <div class="mb-2">
-        <label for="ConfirmPassword" class="form-label">current Password</label>
-        <input type="password" class="form-control" id="Password" minlength="8" required>
+        <label for="currentPassword" class="form-label">current Password</label>
+        <input type="text" class="form-control" id="currentPassword" minlength="8" required>
     </div>
-    <button type="button" class="btn btn-primary" onclick="AccountSettings()">Update Info</button>
+    <button type="button" class="btn btn-primary" onclick=" updateInfo()">Update Info</button>
 </form> `
 };
-let signedInUser;
 
 function getPageContent(page) {
     var contentToReturn;
@@ -108,7 +125,7 @@ function SignUp() {
         Password: document.getElementById('Password').value
     }
 
-    if (username_validation(formData) && password_validation(formData)) {
+    if (username_validation(formData) && password_validation(formData) && email_validation(formData)) {
         localStorage.setItem(`${formData.Email}`, JSON.stringify(formData));
         getPageContent('LogIn')
     } else {
@@ -118,13 +135,14 @@ function SignUp() {
 }
 // Function For login
 function LogIn() {
-    var Email = document.getElementById("email").value;
-    var userData = JSON.parse(localStorage.getItem(Email));
-
-    var Password = document.getElementById("password").value;
-    console.log(Password)
-    if (Email == userData.Email && Password == userData.Password) {
+    var email = document.getElementById("email").value;
+    var userData = JSON.parse(localStorage.getItem(email));
+    var password = document.getElementById("password").value;
+    if (email == userData.Email && password == userData.Password) {
         signedInUser = document.getElementById("email").value;
+        sessionStorage.setItem(signedInUser, JSON.stringify(userData))
+        var user = JSON.parse(sessionStorage.getItem(signedInUser));
+        console.log(user.Password);
         DashBoard();
 
     } else {
@@ -132,7 +150,7 @@ function LogIn() {
     }
 
 }
-
+// Function For validations applied on Name
 function username_validation(data) {
     var validate_name = /^[A-Za-z]+$/;
     if (data.Name.match(validate_name) && data.LastName.match(validate_name)) {
@@ -145,7 +163,7 @@ function username_validation(data) {
         return false;
     }
 }
-
+// Function For validations applied on password
 function password_validation(data) {
     var validate_pass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/;
     let ConfirmPassword = document.getElementById("ConfirmPassword").value;
@@ -153,6 +171,7 @@ function password_validation(data) {
     if (data.Password.match(validate_pass)) {
         document.querySelector('#Password').classList.add("success");
         if (data.Password == ConfirmPassword) {
+            document.getElementById("ConfirmPassword").classList.add("success");
             message.textContent = "Passwords match";
             message.style.backgroundColor = "#1dcd59";
             return true;
@@ -167,22 +186,94 @@ function password_validation(data) {
 
     }
 }
+// Function For validations applied on password
+function email_validation(data) {
+    let validateEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (data.Email.match(validateEmail)) {
+        document.getElementById("Email").classList.add("success");
+        return true;
+    } else {
+        document.querySelector('#Email').classList.add("failed");
+        return false;
+    }
+}
 
+// Function For All the operations on dashboard
 function DashBoard(data) {
     getPageContent('DashBoard');
     var current_userData = JSON.parse(localStorage.getItem(signedInUser));
     let user = document.getElementById("loggedInUser");
     user.textContent = `Signed in as ${current_userData.Name} `;
 }
-
+// Function For logOut
 function LogOut() {
-    getPageContent('LogIn');
+    sessionStorage.removeItem(signedInUser);
+    getPageContent('home');
 
 }
 
 function AccountSettings() {
+    getPageContent('AccountSettings')
     var current_userinfo = JSON.parse(localStorage.getItem(signedInUser));
+    let user = document.getElementById("loggedInUser");
+    user.textContent = `Signed in as ${current_userinfo.Name} `;
     let message = document.getElementById("Edit_Info");
     message.textContent = `${current_userinfo.Name}'s Account Settings`;
+}
 
+function updateInfo() {
+    //functionality to update User's info
+    let updatedData = {
+        Name: document.getElementById('updateFirstName').value,
+        LastName: document.getElementById('updateLastName').value,
+        Email: document.getElementById('updateEmail').value,
+        Password: document.getElementById('NewPassword').value
+    }
+    if (updated_username_validation(updatedData) && updated_password_validation(updatedData) && updated_email_validation(updatedData)) {
+        //functionality to delete User's previous info
+        sessionStorage.removeItem(signedInUser);
+        sessionStorage.setItem(`${updatedData.Email}`, JSON.stringify(updatedData));
+        signedInUser = document.getElementById("updateEmail").value;
+    } else {
+        alert('enter right info')
+    }
+}
+
+function updated_username_validation(data) {
+    var validate_name = /^[A-Za-z]+$/;
+    console.log(data.Name);
+    if (data.Name.match(validate_name) && data.LastName.match(validate_name)) {
+        document.querySelector('#updateFirstName').classList.add("success");
+        document.querySelector('#updateLastName').classList.add("success");
+        return true;
+    } else {
+        document.querySelector('#updateFirstName').classList.add("failed");
+        document.querySelector('#updateLastName').classList.add("failed");
+        return false;
+    }
+}
+
+function updated_password_validation(data) {
+    var validate_pass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/;
+
+    if (data.Password.match(validate_pass)) {
+        document.querySelector('#NewPassword').classList.add("success");
+        return true;
+    } else {
+        message.textContent = "Password Patern incorrect";
+        message.style.backgroundColor = "#ff4d4d";
+        return false;
+    }
+}
+
+function updated_email_validation(data) {
+    let validateEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (data.Email.match(validateEmail)) {
+        document.getElementById("updateEmail").classList.add("success");
+        return true;
+    } else {
+        document.querySelector('#updateEmail').classList.add("failed");
+        return false;
+    }
 }
